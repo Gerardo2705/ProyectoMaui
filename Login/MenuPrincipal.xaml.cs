ce2591ec82d07401745e3ec52da30e4c252e424e
CHATO <colochocristian72@gmail.com>
2024-09-29 21:50:31 -0600

    Actualizando codigo

--- Login/MenuPrincipal.xaml.cs
+++ Login/MenuPrincipal.xaml.cs
@@ -1,3 +1,6 @@
+using OnlyCars.Autos;
+using OnlyCars.Views;
+
 namespace Login;
 
 public partial class MenuPrincipal : ContentPage
@@ -7,40 +10,19 @@ public partial class MenuPrincipal : ContentPage
 		InitializeComponent();
 	}
 
-    private void Button_Clicked(object sender, EventArgs e)
-    {
-
-    }
-
-    private void Button_Clicked_1(object sender, EventArgs e)
-    {
-
-    }
-
-    private async void Button_Clicked_2(object sender, EventArgs e)
-    {
-        await Navigation.PushAsync(new DeleteUserPage());
-
-    }
-
-    private async void ImageButton_Clicked(object sender, EventArgs e)
-    {
-        await Navigation.PushAsync(new DeleteUserPage());
-    }
-
     private async void ImageButton_Clicked_1(object sender, EventArgs e)
     {
+        await Navigation.PushAsync(new Motos());
+
+    }
+
+    private async void ImageButton_Clicked_2(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new AutosOC());
+    }
+
+    private async void ImageButton_Clicked_3(object sender, EventArgs e)
+    {
         await Navigation.PushAsync(new DeleteUserPage());
-
-    }
-
-    private void ImageButton_Clicked_2(object sender, EventArgs e)
-    {
-
-    }
-
-    private void ImageButton_Clicked_3(object sender, EventArgs e)
-    {
-
     }
 }
